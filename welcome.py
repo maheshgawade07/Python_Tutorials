7ce3b32a604f1c3760a49e9b210429bde75f685d
maheshgawade07 <58648789+maheshgawade07@users.noreply.github.com>
2022-02-06 04:56:09 +0530

    adding a new line to welcome program

--- welcome.py
+++ welcome.py
@@ -1,6 +1,7 @@
 def main():
     print ("welcome to python")
     print ("using a python git repo")
+    print ("adding one more line to test push command")
 
 if __name__ == "__main__":
     main()    
